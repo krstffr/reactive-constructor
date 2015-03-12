8d9795cba4bc931e807b7089e4d717beb9108a0c
Kristoffer Klintberg <krstffr@gmail.com>
2015-03-12 16:26:04 +0100

    Fixed two hound warnings

--- ie9-type-check-monkey-patch.js
+++ ie9-type-check-monkey-patch.js
@@ -3,12 +3,12 @@
 // I got this from:
 // http://stackoverflow.com/questions/332422/how-do-i-get-the-name-of-an-objects-type-in-javascript
 if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
-	Object.defineProperty(Function.prototype, 'name', {
+	Object.defineProperty(Function.prototype, "name", {
 		get: function() {
 			var funcNameRegex = /function\s([^(]{1,})\(/;
 			var results = (funcNameRegex).exec((this).toString());
 			return (results && results.length > 1) ? results[1].trim() : "";
 		},
-		set: function(value) {}
+		set: function() {}
 	});
 }
