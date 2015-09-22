514e70030b7807fd6797e0224509b73d77316293
Kristoffer Klintberg <krstffr@gmail.com>
2015-09-22 13:11:39 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "krstffr:reactive-constructor",
   summary: "Create reactive objects from these constructors.",
-	version: "1.2.3"
+	version: "1.2.4"
 });
 
 Package.onUse(function (api) {
